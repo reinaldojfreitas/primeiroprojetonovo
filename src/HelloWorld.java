6de89824ec2e1cdde0d9d63a495fd390798d1e17
REINALDO JOSE DE FREITAS <reinaldo.freitas@educacao.senac.rio>
2022-07-01 20:32:21 -0300

    Primeiro commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @author reinaldo.freitas
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		// Imprime uma linha
+		System.out.println("Hello World"); // Imprime linha
+		System.out.println("-----------"); 
+	}
+}
