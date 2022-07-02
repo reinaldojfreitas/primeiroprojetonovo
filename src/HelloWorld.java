bee6ed4599d1d09f8e1bc3a4a5f7419982ab5838
REINALDO JOSE DE FREITAS <reinaldo.freitas@educacao.senac.rio>
2022-07-01 21:41:32 -0300

    Atualização Javadoc

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,13 @@
 /**
  * 
  * @author reinaldo.freitas
- *
+ * @see java.lang.String
  */
 public class HelloWorld {
+	/**
+	 * 
+	 * @param args Argumentos a serem passados na linha de comando
+	 */
 	public static void main(String[] args) {
 		// Imprime uma linha
 		System.out.println("Hello World"); // Imprime linha
