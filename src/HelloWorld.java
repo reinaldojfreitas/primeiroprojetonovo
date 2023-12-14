1d0bf9a4743d06aa961d0e76d050c1a135fa4086
Reinaldo Freitas <reinaldo.freitas@rj.senac.br>
2023-12-14 20:31:45 -0300

    Primeiro commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author reinaldo.freitas
+ *
+ */
+public class HelloWorld {
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Primeiro projeto 
+		System.out.println("Hello World!");
+	}
+}
