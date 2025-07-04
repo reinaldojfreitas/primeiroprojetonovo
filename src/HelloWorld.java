78b6ee1e298bf53bd833f190c88248a5f1d35536
Reinaldo Freitas <reinaldo.freitas@rj.senac.br>
2025-07-03 21:42:46 -0300

    Primeiro commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author reinaldo.freitas
+ *
+ */
+public class HelloWorld {
+	/**
+	 * Este é o método main
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+	}
+}
